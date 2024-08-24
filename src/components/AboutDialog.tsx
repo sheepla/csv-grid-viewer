6086d684420b4e29e68e14d6a8dcd2cfdfa10c51
mikoto2000 <mikoto2000@gmail.com>
2024-08-24 11:22:53 +0000

    Format source.

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -12,7 +12,7 @@ import {
   Divider,
 } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import { open as browserOpen} from "@tauri-apps/api/shell";
+import { open as browserOpen } from "@tauri-apps/api/shell";
 
 const AboutDialog: React.FC<{ open: boolean; onClose: () => void }> = ({ open, onClose }) => {
   return (
